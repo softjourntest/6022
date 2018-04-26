7b41c14ff876031d39070d5399c2df60a225fcfd
test <test@test.com>
2018-04-26 13:40:28 +0000

    [Connect] Saved endpoint "client.logingreen"

--- server/client/logingreen.js
+++ server/client/logingreen.js
@@ -1,15 +1,4 @@
-const trigger = require('smartux-connect').trigger;
 exports.submit = async (session, models, vars) => {
     await session.transform['6022'].update('logingreen', models.logingreen);
     await session.transform['6022'].action('logingreen', 'submit');
 };
-trigger({
-    id: 'client.start',
-    events: ['client.start']
-}, async (session, models, vars) => {
-    session.call('', session, models, vars);
-    await session.updateUserInfo();
-    await session.transform['6022'].close();
-    await session.transform['6022'].evalSync();
-    await session.transform['6022'].screenshot();
-});
