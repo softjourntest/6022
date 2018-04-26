2412a46157feb03cfcfc9497e86c75b5736e717c
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-04-26 13:41:23 +0000

    [Studio] Add screen "home"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,14 +2,19 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
 import { logingreen_PhonePortrait } from '../pages/logingreen/PhonePortrait/logingreen';
+import { home_PhonePortrait } from '../pages/home/PhonePortrait/home';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    logingreen_PhonePortrait
+    logingreen_PhonePortrait,
+    home_PhonePortrait
   ];
   static mapping = {
     'logingreen': {
       PhonePortrait: logingreen_PhonePortrait
+    },
+    'home': {
+      PhonePortrait: home_PhonePortrait
     }
   }
 }
