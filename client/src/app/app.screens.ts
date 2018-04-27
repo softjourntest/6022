4cfdf42dfd38923f62d59077aad2b05fb5fd0288
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-04-27 13:06:27 +0000

    [Studio] Delete screen "home"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,19 +2,14 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
 import { logingreen_PhonePortrait } from '../pages/logingreen/PhonePortrait/logingreen';
-import { home_PhonePortrait } from '../pages/home/PhonePortrait/home';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    logingreen_PhonePortrait,
-    home_PhonePortrait
+    logingreen_PhonePortrait
   ];
   static mapping = {
     'logingreen': {
       PhonePortrait: logingreen_PhonePortrait
-    },
-    'home': {
-      PhonePortrait: home_PhonePortrait
     }
   }
 }
