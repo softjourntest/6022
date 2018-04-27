4c8ab99fb7253d979891fe91b1f21d324b0d23b9
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-04-27 11:55:52 +0000

    [Studio] Reverted to "[Connect] Saved endpoint "client.home""

--- server/index.js
+++ server/index.js
@@ -2,4 +2,5 @@ const session = require('smartux-connect');
 session.transform['6022'] = session.loadTransform('6022');
 require('./client');
 require('./client/logingreen');
+require('./client/home');
 session.start();
