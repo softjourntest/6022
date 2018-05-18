60e96d0b133d1480c15d3447c34eb7cde85b556f
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-05-18 11:25:13 +0000

    [Studio] Update client version

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,8 @@
 /***  Generated file, do not change.  */
-import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
 import { logingreen_PhonePortrait } from '../pages/logingreen/PhonePortrait/logingreen';
 export class Screens {
-  static declarations: Page[] = [
+  static declarations = [
     MenuComponent,
     logingreen_PhonePortrait
   ];
