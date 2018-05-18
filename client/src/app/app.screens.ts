91b57f4a4d3c23fdabe47f4a2dff9bd06424b580
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-05-18 12:50:11 +0000

    [Studio] Reverted to "[Connect] Saved endpoint "client.home""

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,20 @@
 /***  Generated file, do not change.  */
+import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
 import { logingreen_PhonePortrait } from '../pages/logingreen/PhonePortrait/logingreen';
+import { home_PhonePortrait } from '../pages/home/PhonePortrait/home';
 export class Screens {
-  static declarations = [
+  static declarations: Page[] = [
     MenuComponent,
-    logingreen_PhonePortrait
+    logingreen_PhonePortrait,
+    home_PhonePortrait
   ];
   static mapping = {
     'logingreen': {
       PhonePortrait: logingreen_PhonePortrait
+    },
+    'home': {
+      PhonePortrait: home_PhonePortrait
     }
   }
 }
